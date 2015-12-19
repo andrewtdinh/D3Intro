a11a9a4fc4801a7e053bbd36195627373f5a6371
Andrew Dinh <andrewdinh74@yahoo.com>
2015-12-19 12:41:07 -0800

    Bind to mouse over event

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var sampleSVG = d3.select("#viz")
+var sampleSVG = d3.select("#simpleCircle")
   .append("svg")
   .attr("width", 100)
   .attr("height", 100);
@@ -12,3 +12,23 @@ sampleSVG.append("circle")
   .attr("cy", 50)
   .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
   .on("mouseout", function(){d3.select(this).style("fill", "white");});
+
+var sampleSVG = d3.select("#aniCircle")
+  .append("svg")
+  .attr("width", 100)
+  .attr("height", 100);
+
+sampleSVG.append("circle")
+    .style("stroke", "gray")
+    .style("fill", "white")
+    .attr("r", 40)
+    .attr("cx", 50)
+    .attr("cy", 50)
+    .on("mouseover", function(){d3.select(this)
+      .transition()
+        .delay(30)
+        .duration(1000)
+        .attr("r", 10)
+        .attr("cx", 30)
+        .style("fill", "blue");
+    })
