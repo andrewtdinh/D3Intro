d18e1826d32d2a927fba14c48c183860f229eb52
Andrew Dinh <andrewdinh74@yahoo.com>
2015-12-20 22:32:13 -0800

    Add animation chaining example

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 'use strict';
+// Simple Circle:
 var sampleSVG = d3.select("#simpleCircle")
   .append("svg")
   .attr("width", 100)
@@ -12,7 +13,7 @@ sampleSVG.append("circle")
   .attr("cy", 50)
   .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
   .on("mouseout", function(){d3.select(this).style("fill", "white");});
-
+// Animate Circle
 var sampleSVG = d3.select("#aniCircle")
   .append("svg")
   .attr("width", 100)
@@ -40,3 +41,28 @@ sampleSVG.append("circle")
         .attr("cx", 50)
         .style("fill", "white");
     })
+
+// Animation chaining
+var sampleSVG = d3.select("#chain")
+    .append("svg")
+    .attr("width", 100)
+    .attr("height", 100);
+
+sampleSVG.append("circle")
+    .style("stroke", "gray")
+    .style("fill", "white")
+    .attr("r", 40)
+    .attr("cx", 50)
+    .attr("cy", 50)
+    .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+    .on("mouseout", function(){d3.select(this).style("fill", "white");})
+    .on("mousedown", animate);
+
+function animate() {
+    d3.select(this).transition()
+        .duration(1000)
+        .attr("r", 10)
+      .transition()
+        .delay(1000)
+        .attr("r", 40);
+};
