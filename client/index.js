70cccf42023cb5775e5593e333b3dd6a4b41e9b0
Andrew Dinh <andrewdinh74@yahoo.com>
2015-12-26 14:59:49 -0800

    Just add code for binding to 2D data

--- client/index.js
+++ client/index.js
@@ -133,3 +133,34 @@ circle.style('fill', 'steelblue');
 circle.data(radii);
 circle.attr('r', function(data){return data;})
 circle.attr('cx', function(data, i){return i * 100 + 30;})
+
+//Binding 2D data:
+var dataset = [],
+tmpDataset = [],
+i, j;
+
+for (i = 0; i < 5; i++) {
+    for (j = 0, tmpDataset = []; j < 3; j++) {
+        tmpDataset.push("Row:"+i+",Col:"+j);
+    }
+    dataset.push(tmpDataset);
+}
+
+d3.select("#viz")
+    .append("table")
+    .style("border-collapse", "collapse")
+    .style("border", "2px black solid")
+
+    .selectAll("tr")
+    .data(dataset)
+    .enter().append("tr")
+
+    .selectAll("td")
+    .data(function(d){return d;})
+    .enter().append("td")
+    .style("border", "1px black solid")
+    .style("padding", "10px")
+    .on("mouseover", function(){d3.select(this).style("background-color", "aliceblue")})
+    .on("mouseout", function(){d3.select(this).style("background-color", "white")})
+    .text(function(d){return d;})
+    .style("font-size", "12px");
